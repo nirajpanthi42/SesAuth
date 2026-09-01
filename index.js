d6c751059ac8cfef58b307263c89d0bb8803679a
Niraj Panthi <nirajpanthi42@gmail.com>
2026-09-01 16:50:09 +0545

    push code base

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require('express');
+
+const app=express();
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send("hello world");
+})
+
+
+
+app.listen(4000,()=>{
+    console.log("server is running on port 4000");
+})
+
